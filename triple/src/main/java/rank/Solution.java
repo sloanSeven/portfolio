a887d220262a7d59a84121e930f491186f270a8e
steve <steve@primitivedynamics.com>
2018-04-27 08:55:23 -0400

    basic hackerrank

---
+++ triple/src/main/java/rank/Solution.java
@@ -0,0 +1,16 @@
+package rank;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String myString = scanner.next();
+		int myInt = scanner.nextInt();
+		scanner.close();
+
+		System.out.println("myString is: " + myString);
+		System.out.println("myInt is: " + myInt);
+	}
+}
